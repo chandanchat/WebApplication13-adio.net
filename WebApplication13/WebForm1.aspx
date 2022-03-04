2a6359ff70ecf897ae76919abf6db12230dcea62
Chandan <Chandan@DESKTOP-7G0P5Q8>
2022-03-04 00:41:57 -0800

    Add project files.

---
+++ WebApplication13/WebForm1.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication13.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                    <tr>
+                        <td>Enter Your Name</td>
+                        <td><asp:TextBox runat="server" ID="txtname"></asp:TextBox></td>
+                    </tr>
+                                    <tr>
+                        <td>Enter Your Age</td>
+                        <td><asp:TextBox runat="server" ID="txtage"></asp:TextBox></td>
+                    </tr>
+                <tr>
+                    <td colspan="2"><asp:Button runat="server" id="btninsert" Text="Save" OnClick="btninsert_Click"/></td>
+                </tr>
+            </table>
+        </div>
+            <table>
+        <tr>
+            <td><asp:GridView ID="grd" runat="server" OnRowCommand="grd_RowCommand" AutoGenerateColumns="false" >
+                <Columns>
+                    <asp:TemplateField HeaderText="Employee Id">
+                        <ItemTemplate>
+                            <%#Eval("id") %>
+                        </ItemTemplate>
+                    </asp:TemplateField >
+                                        <asp:TemplateField HeaderText="Employee Name">
+                        <ItemTemplate>
+                            <%#Eval("name") %>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                                        <asp:TemplateField HeaderText="Employee Age">
+                        <ItemTemplate>
+                            <%#Eval("age") %>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Employee Delete">
+                        <ItemTemplate>
+                             <asp:LinkButton runat="server" ID="btndelete" Text="Delete" CommandName="A" CommandArgument='<%#Eval("id") %>'></asp:LinkButton>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                                        <asp:TemplateField HeaderText="Employee Update">
+                        <ItemTemplate>
+                             <asp:LinkButton runat="server" ID="btnedit" Text="Edit" CommandName="B" CommandArgument='<%#Eval("id") %>' ></asp:LinkButton>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+                </asp:GridView></td>
+           
+        </tr>
+    </table>
+    </form>
+
+</body>
+</html>
---
+++ WebApplication13/WebForm1.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace WebApplication13
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("data source=DESKTOP-7G0P5Q8; initial catalog=koko; integrated security=true");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            EmpGetData();
+        }
+        public void blank()
+        {
+            txtage.Text = "";
+            txtname.Text = "";
+        }
+
+        protected void btninsert_Click(object sender, EventArgs e)
+        {
+            if(btninsert.Text=="Save")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into emp(name,age) values('" + txtname.Text + "','" + txtage.Text + "')", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+                else
+                    {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("update emp set name='"+txtname.Text+"', age='"+txtage.Text+"' where id='"+ ViewState["id"] + "'", con);
+                cmd.ExecuteNonQuery();
+                btninsert.Text = "Save";
+                con.Close();
+               
+            }
+            EmpGetData();
+            blank();
+        }
+        public void EmpGetData()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from emp", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Close();
+            grd.DataSource = dt;
+            grd.DataBind();
+        }
+
+        protected void grd_RowCommand(object sender, GridViewCommandEventArgs e)
+        { 
+            if(e.CommandName=="A")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("delete  from emp where id='" + e.CommandArgument + "'", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                EmpGetData();
+            }
+            else
+            {
+                con.Open();
+               
+                    SqlCommand cmd = new SqlCommand("select *  from emp where id='" + e.CommandArgument + "'", con);
+                    SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                     
+                     txtname.Text = dt.Rows[0]["name"].ToString();
+                    txtage.Text = dt.Rows[0]["age"].ToString();
+                    btninsert.Text = "Update";
+                ViewState["id"] = e.CommandArgument;
+                con.Close();
+
+            }
+        }
+
+    }
+}
